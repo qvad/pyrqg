1ea136f97db28e78f9d7005915ff48d4be0702af
Dmitrii Sherstobitov <sdn.xaoc@gmail.com>
2025-12-19 11:43:56 -0800

    Reworked

--- pyrqg/core/__init__.py
+++ pyrqg/core/__init__.py
@@ -1,16 +1,3 @@
 """Core utilities for the PyRQG CLI."""
-
-from .engine import Engine, EngineConfig, EngineStats
-from .validator import ValidatorRegistry
-from .reporter import ReporterRegistry
-from .watchdog import QueryWatchdog, format_sql_multiline
-
-__all__ = [
-    "Engine",
-    "EngineConfig",
-    "EngineStats",
-    "ValidatorRegistry",
-    "ReporterRegistry",
-    "QueryWatchdog",
-    "format_sql_multiline",
-]
+# Legacy components have been removed.
+# This package currently contains execution.py which is used by tests.
